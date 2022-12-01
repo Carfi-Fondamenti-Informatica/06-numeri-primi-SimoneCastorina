b3b8b91138b70f18c74e2c85618ec531e4073efd
SimoneCastorina <115416896+SimoneCastorina@users.noreply.github.com>
2022-12-01 13:01:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  int a=0;
+    cin>>a;
+    if(nprimi(a)==0){
+        cout<<"numero primo"<<endl;
+    }else{
+        cout<<"numero non primo"<<endl;
+    }
   return 0;
 }
